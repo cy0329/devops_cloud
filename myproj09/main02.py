e93be7307aa9764cb4fcfad1f4d666e968e18603
Yeol Choi <chlduf153@gmail.com>
2021-11-09 10:52:02 +0900

    main01 내용변경, 멜론TOP100 리스트에서 "곡명" 단어수로 TOP10 곡명 출력

--- myproj09/main02.py
+++ myproj09/main02.py
@@ -8,12 +8,14 @@ df = pd.read_csv("https://bit.ly/3nsLDXy")
 song_list = list(df.T.to_dict().values())
 
 # 좋아요 수로 TOP10 곡명 리스트를 만들어봅시다.
-def pick_like_value(song_dict):
-    return song_dict["like"]
+def pick_word_count_for_title(song_dict):
+    title: str = song_dict["title"]
+    word_list = title.split()
+    return len(word_list)
 
 
 sorted_song_list = sorted(
-    song_list, key=pick_like_value, reverse=True
+    song_list, key=pick_word_count_for_title, reverse=True
 )  # +[:10]: 를 in 뒤에 넣어도 됨
 top10_song_list = sorted_song_list[:10]
 
