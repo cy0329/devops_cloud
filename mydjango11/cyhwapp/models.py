686de9b4b7521829958ddb10aafec6513b08b2e8
Yeol Choi <chlduf153@gmail.com>
2021-11-29 19:33:48 +0900

    과제 제출

--- mydjango11/cyhwapp/models.py
+++ mydjango11/cyhwapp/models.py
@@ -4,8 +4,9 @@ class Badmintonplace(models.Model):
     name = models.CharField(max_length=50, verbose_name='이름')
     address = models.CharField(max_length=100, verbose_name='주소')
     phone_num = models.CharField(max_length=11, verbose_name='연락처')
-    homepage = models.CharField(max_length=200, verbose_name='홈페이지')
-    email = models.CharField(max_length=50, verbose_name='이메일')
+    homepage = models.CharField(max_length=200, verbose_name='홈페이지', blank=True, null=True)
+    email = models.CharField(max_length=50, verbose_name='이메일', blank=True, null=True)
+    parking = models.BooleanField(verbose_name='주차장 보유 여부')
 
 
     def __str__(self):
