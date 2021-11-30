dd71899e6ff37c1f30d5d9f8a871de65b89ed48e
Yeol Choi <chlduf153@gmail.com>
2021-11-30 19:55:21 +0900

    7 과정(의미있는 데이터 10건 추가)

--- myhomework11/mintonplace/models.py
+++ myhomework11/mintonplace/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+import datetime
 
 class mintongym(models.Model):
     place_name = models.CharField(max_length=100, verbose_name='민턴장 이름')
     address = models.CharField(max_length=100, verbose_name='주소')
-    contact = models.IntegerField(verbose_name='연락처')
+    contact = models.CharField(max_length=11, verbose_name='연락처', blank=True)
     parking = models.BooleanField(verbose_name='주차장 보유')
-    running_time = models.TextField(verbose_name='운영시간 및 설명')
+    starttime = models.TimeField(verbose_name='시작 시간', default=datetime.time(), blank=True)
+    endtime = models.TimeField(verbose_name='종료 시간', default=datetime.time(), blank=True)
+    explain = models.TextField(verbose_name='설명', blank=True)
+
+    def __str__(self):
+        return self.place_name
