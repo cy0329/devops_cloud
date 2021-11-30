052460aeebe0438ec79ae40d2ab7f16fb6a3162b
Yeol Choi <chlduf153@gmail.com>
2021-11-30 18:28:14 +0900

    5 과정(모델 생성, migration)

--- myhomework11/mintonplace/models.py
+++ myhomework11/mintonplace/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class mintongym(models.Model):
+    place_name = models.CharField(max_length=100, verbose_name='민턴장 이름')
+    address = models.CharField(max_length=100, verbose_name='주소')
+    contact = models.IntegerField(verbose_name='연락처')
+    parking = models.BooleanField(verbose_name='주차장 보유')
+    running_time = models.TextField(verbose_name='운영시간 및 설명')
