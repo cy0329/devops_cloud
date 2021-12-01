65d114a90a2a1085dc8ff0ada40a172c5e3ab8b3
Yeol Choi <chlduf153@gmail.com>
2021-12-01 19:59:15 +0900

    skiresort/index view 구현

--- myhomework13/myhomework13/urls.py
+++ myhomework13/myhomework13/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from skiresort.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
