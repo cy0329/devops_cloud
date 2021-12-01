3c70a4806faf9e22c38339b9ef3cd9fed96b7c71
Yeol Choi <chlduf153@gmail.com>
2021-12-01 18:32:52 +0900

    skiresort/Resort 모델 및 마이그레이션 생성

--- myhomework13/skiresort/models.py
+++ myhomework13/skiresort/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Resort(models.Model):
+    place_name = models.CharField(max_length=100, verbose_name='스키장 이름')
+    address = models.CharField(max_length=100, verbose_name='주소')
+    contact = models.CharField(max_length=13, blank=True, verbose_name='연락처')
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='등록')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정')
