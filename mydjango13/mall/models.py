90ef4765e88c4ca5833242402d72303c83f2bd69
Yeol Choi <chlduf153@gmail.com>
2021-12-01 13:15:01 +0900

    mall/models에 telephone필드 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -5,7 +5,8 @@ class Shop(models.Model):
     # id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+    open_time = models.TimeField()
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
