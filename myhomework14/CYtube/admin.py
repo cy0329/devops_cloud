bc11c2e87b8843061e635bdfc98ba42641dc7ee7
Yeol Choi <chlduf153@gmail.com>
2021-12-02 19:40:14 +0900

    admin 등록

--- myhomework14/CYtube/admin.py
+++ myhomework14/CYtube/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from CYtube.models import Cyvlog
 
-class CyvlogAdmin():
+class CyvlogAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Cyvlog)
+admin.site.register(Cyvlog, CyvlogAdmin)
