dd753d61bb4b8ec54e42058dfb978485cb8e34b2
Yeol Choi <chlduf153@gmail.com>
2021-12-02 18:10:10 +0900

    index 뷰 구현

--- myhomework14/myhomework14/urls.py
+++ myhomework14/myhomework14/urls.py
@@ -13,17 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from CYtube.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path('CYtube/', index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root='MEDIA_ROOT')
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
