cfe276c2f3291af247480dfd2c94059f47589ce7
Yeol Choi <chlduf153@gmail.com>
2021-12-04 23:14:00 +0900

    4. MEDIA 세팅

--- myhomework15/myhomework15/urls.py
+++ myhomework15/myhomework15/urls.py
@@ -11,3 +11,6 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
