b8b4e966ce8acfcdd1256ee141fdf1686cfa10ff
Yeol Choi <chlduf153@gmail.com>
2021-12-04 23:29:15 +0900

    6. 모델 생성, 마이그레이션

--- myhomework15/badminton/models.py
+++ myhomework15/badminton/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Minton(models.Model):
+    name = models.CharField(max_length=50, verbose_name='민턴장 이름')
+    address = models.CharField(max_length=200, verbose_name='주소')
+    telephone = models.CharField(blank=True, max_length=15, validators=[
+        RegexValidator(r"^\d{3,4}-?\d{3,4}-?\d{4}$",
+                       message="전화번호를 입력해주세요."),
+    ])
+    photo = models.ImageField(blank=True, verbose_name='사진')
+    description = models.TextField(verbose_name='설명')
+    parking = models.BooleanField(default=False, verbose_name='주차장 보유')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- myhomework15/myhomework15/urls.py
+++ myhomework15/myhomework15/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from myhomework15 import settings
