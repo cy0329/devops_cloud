0f54f714b8c784864613817b214f2c0d89f94720
Yeol Choi <chlduf153@gmail.com>
2021-12-04 21:03:50 +0900

    6. 모델 생성 및 마이그레이션

--- myhomework15/badminton/models.py
+++ myhomework15/badminton/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Minton(models.Model):
+    name = models.CharField(max_length=50, verbose_name="체육관명")
+    description = models.TextField(verbose_name="설명")
+    photo = models.ImageField(verbose_name="사진", blank=True)
+    # open_at = models.TimeField(auto_now=True)
+    parking = models.BooleanField(blank=True, verbose_name="주차장 보유")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
