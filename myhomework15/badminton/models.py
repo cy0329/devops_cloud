5d74717deb73f401f1cca1abbd2a8c5c6b9cd55c
Yeol Choi <chlduf153@gmail.com>
2021-12-04 21:21:44 +0900

    + models 필드 추가/변경

--- myhomework15/badminton/models.py
+++ myhomework15/badminton/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Minton(models.Model):
     name = models.CharField(max_length=50, verbose_name="체육관명")
+    address = models.CharField(max_length=100, verbose_name="주소", blank=True)
+    telephone = models.CharField(max_length=13, verbose_name="연락처", blank=True)
     description = models.TextField(verbose_name="설명")
     photo = models.ImageField(verbose_name="사진", blank=True)
     # open_at = models.TimeField(auto_now=True)
