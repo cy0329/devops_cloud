9352d828563bc366c8c5a6ca009ab789d0ada7b4
Yeol Choi <chlduf153@gmail.com>
2021-12-06 23:02:57 +0900

    4. MEDIA 세팅

--- myhomework16/myhomework16/urls.py
+++ myhomework16/myhomework16/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,6 +22,8 @@ from myhomework16 import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
