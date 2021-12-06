7a586f5b90925eb08cfc022c9f52866e956ed56a
Yeol Choi <chlduf153@gmail.com>
2021-12-07 00:03:08 +0900

    7~9. 부트스트랩 적용, 템플릿 상속기능, _list, _detail 구현

--- myhomework16/badminton/views.py
+++ myhomework16/badminton/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from badminton.models import Minton
 
-# Create your views here.
+
+def minton_list(request: HttpRequest) -> HttpResponse:
+    qs = Minton.objects.all()
+    template_name = 'badminton/minton_list.html'
+    context_data = {
+        'minton_list': qs,
+    }
+    return render(request, template_name, context_data)
+
+def minton_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    minton = Minton.objects.get(pk=pk)
+    template_name = 'badminton/minton_detail.html'
+    context_data = {
+        'minton': minton,
+    }
+    return render(request, template_name, context_data)
+
