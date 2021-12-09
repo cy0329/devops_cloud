db12c05c420d3f5638a8993a272f29e015ed37ec
Yeol Choi <chlduf153@gmail.com>
2021-12-09 13:57:28 +0900

    post_edit 뷰 구현

--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -38,26 +38,35 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
 
 
 def post_new(request: HttpRequest) -> HttpResponse:
-    # print("request.method :", request.method)
-    # print("request.GET :", request.GET)
-    # print("request.POST :", request.POST)
-    # print("request.FILES :", request.FILES)
-
-    # 입력 서식을 보여주겠다.
-    if request.method == "GET":
-        form = PostForm()
-    # 서식 입력값을 전달 받아서, 유효성 검사를 하겠다.
-    # -> 에러 상황에서는 에러메시지를 보여주겠다.
-    # -> 유효성 검사에 통과하면, 입력값을 보여주고, post_list로 이동
-    else:
+    if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            print("유효성 검사에 통과했습니다. :", form.cleaned_data)
-            form.save()  # ModelForm에서만 지원
-            return redirect("diary:post_list")
-        else:
-            pass
+            form.save()
+            return redirect('diary:post_list')
+    else:
+        form = PostForm()
 
     return render(request, "diary/post_form.html", {
         "form": form,
     })
+
+
+# 수정도 detail처럼 수정대상을 지정하기 위해 pk를 인자로 받아줘야함
+def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    # 아래 코드는 ModelForm에 한해서 동작하는 코드
+    post = Post.objects.get(pk=pk)
+
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        # ModelForm이기에 instance 넣는게 가능
+        if form.is_valid():
+            form.save()
+            return redirect('diary:post_list')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "diary/post_form.html", {
+        "form": form,
+    })
+    # pass라고 쓰는것이나 return None이나 같음 -> Response가 없다는 오류 뜸
