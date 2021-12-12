f061fd57fdf7ba1f0c34d3cefd8f811452b688c2
Yeol Choi <chlduf153@gmail.com>
2021-12-12 21:03:57 +0900

    20과제04) post_form 뷰 구현 + bootstrap5 스타일 적용

---
+++ myhomework20/mintonplace/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from mintonplace.models import Post, Review
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
