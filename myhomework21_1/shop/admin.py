8d89341ba0bbf5d36e283fb1547489fcb8ba8e50
Yeol Choi <chlduf153@gmail.com>
2021-12-13 21:00:45 +0900

    21_1연습04) model만들고 admin 등록

--- myhomework21_1/shop/admin.py
+++ myhomework21_1/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Shop, Tag, Category
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'address', 'telephone']
+    list_display_links = ['name']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
--- myhomework21_1/shop/models.py
+++ myhomework21_1/shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+
+class Shop(TimestampedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    address = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=15, validators=[RegexValidator(r"^\d{3}-?\d{3,4}-?\d{4}", message="전화번호를 입력해주세요."),])
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
