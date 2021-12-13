fadac3dc6a87f57373da6faeb913c902ee936b4d
Yeol Choi <chlduf153@gmail.com>
2021-12-14 00:43:28 +0900

    21과제04) 모델 설정, admin 등록

--- myhomework21_2/shop/admin.py
+++ myhomework21_2/shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Shop, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
