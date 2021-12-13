5462f4de6ed86f28d302db4a59972cbe71523e94
Yeol Choi <chlduf153@gmail.com>
2021-12-14 01:46:03 +0900

    21과제13) Tag 저장 로직을 ShopForm클래스로 이동, tags 필드 초기값을 구현

--- myhomework21_2/shop/forms.py
+++ myhomework21_2/shop/forms.py
@@ -1,10 +1,32 @@
 from django import forms
 
-from shop.models import Shop
+from shop.models import Shop, Tag
 
 
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            tag_qs = self.instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
+        else:
+            pass
+
+    def save(self):
+        saved_post = super().save()
+
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        saved_post.tag_set.clear()
+        saved_post.tag_set.add(*tag_list)
 
     class Meta:
         model = Shop
