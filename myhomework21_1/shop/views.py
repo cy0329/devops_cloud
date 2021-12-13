91db23695687792bcd5c3cdecd0c9de76db77af6
Yeol Choi <chlduf153@gmail.com>
2021-12-13 23:24:58 +0900

    21_1연습07) shop_edit 뷰 구현, detail에서 연결

--- myhomework21_1/shop/views.py
+++ myhomework21_1/shop/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from shop.forms import ShopForm
 from shop.models import Shop
@@ -30,4 +30,19 @@ def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
     shop = Shop.objects.get(pk=pk)
     return render(request, 'shop/shop_detail.html', {
         'shop': shop,
+    })
+
+
+def shop_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            form.save()
+            return redirect('shop:shop_detail', pk)
+    else:
+        form = ShopForm(instance=shop)
+
+    return render(request, 'shop/shop_form.html', {
+        'form': form,
     })
