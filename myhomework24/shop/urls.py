b2360caf838926eb7e49451b5c5712d001fc30d5
Yeol Choi <chlduf153@gmail.com>
2021-12-16 19:53:41 +0900

    7. shop_new 뷰 구현

--- myhomework24/shop/urls.py
+++ myhomework24/shop/urls.py
@@ -5,5 +5,6 @@ from shop import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.shop_list, name='shop_list'),
+    path('new/', views.shop_new, name='shop_new'),
 ]
 
--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from shop.forms import ShopForm
 from shop.models import Shop
 
 
@@ -8,5 +9,18 @@ def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
     return render(request, 'shop/shop_list.html', {
         'shop_list': qs,
+    })
+
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('shop:shop_list')
+    else:
+        form = ShopForm()
+    return render(request, 'shop/shop_form.html', {
+        'form': form,
     })
 
