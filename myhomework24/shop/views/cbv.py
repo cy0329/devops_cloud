3873dfb8f9a496f2ed72bbab776c9becfc00f2c7
Yeol Choi <chlduf153@gmail.com>
2021-12-16 23:06:25 +0900

    14. shop_list, detail, new, edit, delete를 cbv로 기본 구현

---
+++ myhomework24/shop/views/cbv.py
@@ -0,0 +1,31 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from shop.forms import ShopForm
+from shop.models import Shop
+
+shop_list = ListView.as_view(
+    model=Shop,
+)
+
+
+shop_detail = DetailView.as_view(
+    model=Shop,
+)
+
+shop_new = CreateView.as_view(
+    model=Shop,
+    form_class=ShopForm,
+    success_url=reverse_lazy('shop:shop_list')
+)
+
+shop_edit = UpdateView.as_view(
+    model=Shop,
+    form_class=ShopForm,
+    success_url=reverse_lazy('shop:shop_list')
+)
+
+shop_delete = DeleteView.as_view(
+    model=Shop,
+    success_url=reverse_lazy('shop:shop_list')
+)
