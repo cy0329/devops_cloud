825c6699d7af6e556006a6966dab7efe3f5e461b
Yeol Choi <chlduf153@gmail.com>
2021-12-17 14:31:57 +0900

    shop_list 뷰를 CBV로 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from shop.models import Shop, Category
+
+
+class ShopListView(ListView):
+    model = Shop
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["category_list"] = Category.objects.all()
+        return context_data
+
+
+shop_list = ShopListView.as_view()  # 클래스를 직접 호출하는건 없음 ==> 클래스.as_view로 호출
