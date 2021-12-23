aa70aeddafd66145763f966b13341efc513209ab
Yeol Choi <chlduf153@gmail.com>
2021-12-23 14:45:18 +0900

    antd 적용

--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -2,8 +2,12 @@
 // 전역 변수 : 바뀌지 않는 데이터
 
 import { useState } from 'react';
+import { Button as AntdButton } from 'react-bootstrap';
+import { Button as BootstrapButton } from 'antd';
 import initialSongList from './data/melon_data.json';
 import './MelonTop100.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
 
 //    - 초기값을 정의
 
@@ -17,10 +21,19 @@ function MelonTop100() {
   return (
     <div>
       <h2>멜론 top 100</h2>
-      <button onClick={handleClick}>로딩</button>
+      <AntdButton variant="success" onClick={handleClick}>
+        로딩
+      </AntdButton>
+      <BootstrapButton type="primary" onClick={handleClick}>
+        로딩
+      </BootstrapButton>
       <ul className="songList">
         {songList.map((song) => {
-          return <li>{song.title}</li>;
+          return (
+            <li key={song.song_no}>
+              {song.rank} {song.title} by {song.artist} {song.like} likes
+            </li>
+          );
         })}
       </ul>
     </div>
