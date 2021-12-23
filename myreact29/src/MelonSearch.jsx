5de496c1fc48d8515d75d740af2345e2b371657b
Yeol Choi <chlduf153@gmail.com>
2021-12-23 18:51:34 +0900

    멜론서치 리스트 디자인 추가

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -1,8 +1,14 @@
 import { Input } from 'antd';
 import { useState } from 'react';
 
+import { List, Avatar } from 'antd';
+
+import Axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
+
 function MelonSearch() {
   const [query, setQuery] = useState('');
+  const [songList, setSongList] = useState([]);
 
   const handleChange = (e) => {
     const {
@@ -13,11 +19,41 @@ function MelonSearch() {
     console.groupEnd();
     setQuery(value);
   };
-  const handlePressEnter = (e) => {
+  const handlePressEnter = () => {
     console.group('handlePressEnter');
     console.log(`검색어 ${query}(으)로 검색합니다.`);
     console.groupEnd();
+
+    const url = 'https://www.melon.com/search/keyword/index.json';
+
+    Axios({
+      url: url,
+      adapter: jsonpAdapter,
+      callbackParamName: 'jscallback',
+      params: {
+        query: query,
+      },
+    })
+      .then((response) => {
+        // ALBUMCONTENTS, ARTISTCONTENTS
+        const {
+          data: { SONGCONTENTS: searchedSongList },
+        } = response;
+        console.group('멜론 검색결과');
+        console.log(response);
+        console.log(searchedSongList);
+        console.groupEnd();
+
+        setSongList(searchedSongList);
+      })
+      .catch((error) => {
+        console.group('멜론 검색 에러');
+        console.log(error);
+        console.groupEnd();
+      });
   };
+
+  const data = [{ title: '' }];
   return (
     <div style={{ width: 300, margin: '0 auto' }}>
       <h2>멜론 검색</h2>
@@ -27,8 +63,31 @@ function MelonSearch() {
         onChange={handleChange}
         onPressEnter={handlePressEnter}
       />
+      {songList.map((song) => (
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={() => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src={song.ALBUMIMG} />}
+                title={song.SONGNAME}
+                description={song.ARTISTNAME}
+              />
+            </List.Item>
+          )}
+        />
+      ))}
     </div>
   );
 }
+
+//        <div>
+//          <img src={song.ALBUMIMG} />
+//          {song.SONGNAME} by {song.ARTISTNAME}
+//        </div>
+//      );
+//    })}
+//  </div>
 
 export default MelonSearch;
