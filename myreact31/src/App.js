c3ce3fd7d51447e4c2d91228e904577cb0d1c50e
Yeol Choi <chlduf153@gmail.com>
2021-12-27 21:30:26 +0900

    myreact31 프로필카드 JSON파일로 변환한 데이터 사용

--- myreact31/src/App.js
+++ myreact31/src/App.js
@@ -6,54 +6,29 @@ import mem1 from "./img/member1.jpg";
 import mem2 from "./img/member2.jpg";
 import mem3 from "./img/member3.jpg";
 import mem4 from "./img/member4.jpg";
+import members from "./data/profiles.json";
 
 function App() {
   // PageProfileCard 에서 인자로써 받아서 버튼으로...?
-  const [pageName, setPageName] = useState("mempf1");
+  const [pageName, setPageName] = useState("Julia");
   return (
     <>
       <h2>리액트 학습</h2>
       <PageLotto />
-      {pageName === "mempf1" && (
-        <ProfileCard
-          profileImage={mem1}
-          name="Julia"
-          role="UI/UX Developer"
-          facebook_url="facebook.com/juliafb1"
-          email="juuulia@naver.com"
-          changePage={setPageName}
-        />
-      )}
-      {pageName === "mempf2" && (
-        <ProfileCard
-          profileImage={mem2}
-          name="Romanov"
-          role="GUI Designer"
-          facebook_url="facebook.com/romav"
-          email="rooomanv@naver.com"
-          changePage={setPageName}
-        />
-      )}
-      {pageName === "mempf3" && (
-        <ProfileCard
-          profileImage={mem3}
-          name="Tom"
-          role="3D Graphic Designer"
-          facebook_url="facebook.com/tommy_79"
-          email="ttoommy@naver.com"
-          changePage={setPageName}
-        />
-      )}
-      {pageName === "mempf4" && (
-        <ProfileCard
-          profileImage={mem4}
-          name="Jackson"
-          role="CPU Builder"
-          facebook_url="facebook.com/jackson"
-          email="jksn123@naver.com"
-          changePage={setPageName}
-        />
-      )}
+      {members.map((member) => {
+        if (pageName === member.name) {
+          return (
+            <ProfileCard
+              name={member.name}
+              role={member.role}
+              facebook_url={member.facebook_url}
+              email={member.email}
+              profileImage={mem1}
+              changePage={setPageName}
+            />
+          );
+        }
+      })}
     </>
   );
 }
--- myreact31/src/pages/PageProfileCard.jsx
+++ myreact31/src/pages/PageProfileCard.jsx
@@ -1,54 +1,53 @@
 import { useState } from "react";
 import "./PageProfileCard.css";
 // import mem1jpg from "./img/member1.jpg";
+// import "../data/profiles.json";
 
 function ProfileCard({
-  profileImage,
   name,
   role,
   facebook_url,
   email,
+  profileImage,
   changePage,
 }) {
   return (
-    <body>
-      <section>
-        <nav className="menu">
-          <a href="#">
-            <i className="fas fa-bars"></i>
-          </a>
-          <a href="#">
-            <i className="far fa-sticky-note"></i>
-          </a>
-        </nav>
-        <article className="profile">
-          <img src={profileImage} alt="프로필 이미지" />
+    <section>
+      <nav className="menu">
+        <a href="#">
+          <i className="fas fa-bars"></i>
+        </a>
+        <a href="#">
+          <i className="far fa-sticky-note"></i>
+        </a>
+      </nav>
+      <article className="profile">
+        <img src={profileImage} alt="프로필 이미지" />
 
-          <h1>{name}</h1>
-          <h2>{role}</h2>
+        <h1>{name}</h1>
+        <h2>{role}</h2>
 
-          <a href="#" className="btnView">
-            VIEW MORE
-          </a>
-          <ul className="contact">
-            <li>
-              <i className="fab fa-facebook-f"></i>
-              <span>{facebook_url}</span>
-            </li>
-            <li>
-              <i className="fas fa-envelope"></i>
-              <span>{email}</span>
-            </li>
-          </ul>
-        </article>
-        <nav className="others">
-          <a onClick={() => changePage("mempf1")}></a>
-          <a onClick={() => changePage("mempf2")}></a>
-          <a onClick={() => changePage("mempf3")}></a>
-          <a onClick={() => changePage("mempf4")}></a>
-        </nav>
-      </section>
-    </body>
+        <a href="#" className="btnView">
+          VIEW MORE
+        </a>
+        <ul className="contact">
+          <li>
+            <i className="fab fa-facebook-f"></i>
+            <span>{facebook_url}</span>
+          </li>
+          <li>
+            <i className="fas fa-envelope"></i>
+            <span>{email}</span>
+          </li>
+        </ul>
+      </article>
+      <nav className="others">
+        <a onClick={() => changePage("Julia")}></a>
+        <a onClick={() => changePage("Romanov")}></a>
+        <a onClick={() => changePage("Tom")}></a>
+        <a onClick={() => changePage("Jackson")}></a>
+      </nav>
+    </section>
   );
 }
 
