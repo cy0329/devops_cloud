660b8f4527bbc45f534d56cc6406cc023163cc9d
Yeol Choi <chlduf153@gmail.com>
2021-12-29 14:51:29 +0900

    myreact33 stage04, 05 Axios 사용, 클리어버튼

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function PageProfile() {
   const [pageList, setPageList] = useState([]);
+  useEffect(() => {
+    Axios.get(
+      'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
+    )
+      .then((response) => {
+        const profileList = response.data.map((profile) => ({
+          ...profile,
+          profileImageUrl: profile.profile_image_url,
+          instagramUrl: profile.instagram_url,
+        }));
+        setPageList(profileList);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div>
       <h2>Page Profile</h2>
-      {pageList.length === 0 ? <h4>등록된 프로필이 없습니다.</h4> : <h4></h4>}
+      <button onClick={() => setPageList([])}>clear</button>
+      <button onClick={}>refresh</button>
+      {pageList.length === 0 && <h4>등록된 프로필이 없습니다.</h4>}
       {pageList.map((member) => (
         <>
           <h3>{member.name}</h3>
