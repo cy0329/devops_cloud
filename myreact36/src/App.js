11dca836a711e6db23ce590c0aeacfbd6ccba14c
Yeol Choi <chlduf153@gmail.com>
2022-01-04 20:10:10 +0900

    36과제 title 속성값 유의미하게 사용하기

--- myreact36/src/App.js
+++ myreact36/src/App.js
@@ -2,7 +2,7 @@ import SevenNumbers2 from 'SevenNumbers2';
 import './App.css';
 
 function App() {
-  return <SevenNumbers2 />;
+  return <SevenNumbers2 title="로또 공 뽑기" />;
 }
 
 export default App;
