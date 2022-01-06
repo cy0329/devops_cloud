e5c1882a38ba81ddd6fcfa281144f5fd1df16f3a
Yeol Choi <chlduf153@gmail.com>
2022-01-07 00:29:19 +0900

    input 값 받아서 todoList에 추가하는 기능 구현

--- mystudy37/src/components/TodoList.jsx
+++ mystudy37/src/components/TodoList.jsx
@@ -9,15 +9,34 @@ const INITIAL_STATE = [
 
 function TodoList() {
   const [state, setState] = useState(INITIAL_STATE);
+  const [inputText, setInputText] = useState('');
+
   const removeTodo = (clickedIndex) => {
     setState((prevState) =>
       prevState.filter((_, index) => index !== clickedIndex),
     );
   };
 
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const saveTodo = (e) => {
+    if (e.key === 'Enter') {
+      setState((prevState) => [...prevState, { content: inputText }]);
+      setInputText('');
+    }
+  };
+
   return (
     <div>
       <h2>Todo List</h2>
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleChange}
+        onKeyPress={saveTodo}
+      />
       {state.map((todo, index) => (
         <Todo todo={todo} onClick={() => removeTodo(index)} />
       ))}
